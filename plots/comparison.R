a634a6204eefa439d1118c98ba427bdfd9c34e0a
Jake Lever <jlever@bcgsc.ca>
2017-02-07 18:10:19 -0800

    Added first figure plot

---
+++ plots/comparison.R
@@ -0,0 +1,17 @@
+
+library(lattice)
+
+args <- commandArgs(TRUE)
+
+inData <- args[1]
+outPlot <- args[2]
+
+data <- read.table(inData)
+
+data <- data[,c(1,3)]
+colnames(data) <- c("Method","AreaUnderPRCurve")
+
+tiff(outPlot)
+barchart( AreaUnderPRCurve ~ Method, data, col="black")
+dev.off()
+
