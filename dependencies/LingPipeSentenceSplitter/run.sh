95e09e3867eb9f8d94545c33909fa9ac2f0484f2
Jake Lever <jlever@bcgsc.ca>
2017-02-08 00:46:45 -0800

    Some unused memory options for running Lingpipe

--- dependencies/LingPipeSentenceSplitter/run.sh
+++ dependencies/LingPipeSentenceSplitter/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 lingpipeJar="lingpipe-4.1.0.jar"
+#export _JAVA_OPTIONS="-Xmx256M"
 
 # Check for Lingpipe in CLASSPATH
 lingpipeSearchCount=`echo "$CLASSPATH" | tr ':' '\n' | xargs -I PATH basename PATH | grep -F -x $lingpipeJar | wc -l`
@@ -11,6 +12,7 @@ fi
 
 HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+#java -Xmx256M -classpath $CLASSPATH:$HERE LingpipeSentenceSplitter
 java -classpath $CLASSPATH:$HERE LingpipeSentenceSplitter
 
 
