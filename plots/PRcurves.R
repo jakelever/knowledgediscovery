1c86a1137afe61a9964a7e5fba5d36e35b2791a5
Jake Lever <jlever@bcgsc.ca>
2017-02-16 13:54:21 -0800

    Added PR curve script

---
+++ plots/PRcurves.R
@@ -0,0 +1,18 @@
+
+library(lattice)
+
+args <- commandArgs(TRUE)
+
+inData <- args[1]
+outPlot <- args[2]
+
+data <- read.table('curves.all')
+
+colnames(data) <- c('analysisName', 'true_positive_rate', 'false_positive_rate', 'adjusted_precision', 'adjusted_f1Score', 'tp', 'fp', 'tn', 'fn', 'threshold')
+
+data <- data[order(data$true_positive_rate,data$adjusted_precision),]
+
+tiff(outPlot)
+xyplot(adjusted_precision~true_positive_rate,data,groups=analysisName,type="l",xlim=c(0,1),ylim=c(0,1),auto.key=T)
+dev.off()
+
