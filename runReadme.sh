1c459d2b3ba819318e5c238c438f1a5ab9bd0707
Jake Lever <jlever@bcgsc.ca>
2017-02-20 21:31:29 -0800

    Added -u and pipefail to README run

--- runReadme.sh
+++ runReadme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "set -ex" > README.sh
+echo "set -euxo pipefail" > README.sh
 cat README.md | awk -F '' ' { if ($0=="```") code=0; if (code==1 || $1 == "#") print $0"\n"; if ($0=="```bash") code=1;}' >> README.sh
 
 bash README.sh
