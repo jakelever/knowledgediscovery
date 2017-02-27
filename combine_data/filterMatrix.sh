73cc686b23ff5a4787ff98d27170da3ff2286438
Jake Lever <jlever@bcgsc.ca>
2017-02-27 11:21:44 -0800

    Fixed matrix filtering for cases where symmetry is not followed

--- combine_data/filterMatrix.sh
+++ combine_data/filterMatrix.sh
@@ -8,7 +8,7 @@ outMatrix=$4
 
 cat $inMatrix |\
 awk -v f=$occurrences -F $'\t' ' BEGIN { while (getline < f) { entities[$1]=1; } } { key=$1"\t"$2; if ( ($1 in entities) && ($2 in entities) ) print $0; } '  |\
-awk -v f=$prevMatrix -F $'\t' ' BEGIN { while (getline < f) { key=$1"\t"$2; rels[key]=1; } } { key=$1"\t"$2; if ( !(key in rels) ) print $0; } '  > $outMatrix
+awk -v f=$prevMatrix -F $'\t' ' BEGIN { while (getline < f) { rels[$1"\t"$2]=1; rels[$2"\t"$1]=1; } } { key=$1"\t"$2; if ( !(key in rels) ) print $0; } '  > $outMatrix
 
 # Sort the output
 sort -k1,1n -k2,2n $outMatrix > $outMatrix.tmp
