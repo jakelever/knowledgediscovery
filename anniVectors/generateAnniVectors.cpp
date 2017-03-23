1c27db2f406d5424914ad733e5e9dc7efe037e72
Jake Lever <jlever@bcgsc.ca>
2017-03-23 01:07:25 -0700

    ANNI vectors now double precision

--- anniVectors/generateAnniVectors.cpp
+++ anniVectors/generateAnniVectors.cpp
@@ -67,30 +67,30 @@ struct Data
 };
 
 
-float H1(int i, 
+double H1(int i, 
 		std::unordered_map<int,int> *occurrences, 
 		int sentenceCount)
 {
-	float N_i = (float)(*occurrences)[i];
-	float N = (float)sentenceCount;
-	float score = -(N_i/N) * log(N_i/N) - ((N-N_i)/N) * log((N-N_i)/N);
+	double N_i = (double)(*occurrences)[i];
+	double N = (double)sentenceCount;
+	double score = -(N_i/N) * log(N_i/N) - ((N-N_i)/N) * log((N-N_i)/N);
 	return score;
 }
 
-float H2(int i, int j, 
+double H2(int i, int j, 
 		std::unordered_map<std::tuple<int,int>,int> *cooccurrences,
 		std::unordered_map<int,int> *occurrences,
 		int sentenceCount)
 {
 	auto key = std::make_tuple ( min(i,j), max(i,j) );
 
-	float N_ij = 0.0f;
+	double N_ij = 0.0f;
 	if (cooccurrences->count(key) > 0)
-		N_ij = (float)(*cooccurrences)[key];
+		N_ij = (double)(*cooccurrences)[key];
 		
-	float N_i = (float)(*occurrences)[i];
-	float N_j = (float)(*occurrences)[j];
-	float N = (float)sentenceCount;
+	double N_i = (double)(*occurrences)[i];
+	double N_j = (double)(*occurrences)[j];
+	double N = (double)sentenceCount;
 
 	int useOld = 0;
 
@@ -103,7 +103,7 @@ float H2(int i, int j,
 		}
 		else
 		{
-			float score = -(N_ij/N) * log(N_ij/N);
+			double score = -(N_ij/N) * log(N_ij/N);
 			score += - ((N_j-N_ij)/N) * log((N_j-N_ij)/N);
 			score += - ((N_i-N_ij)/N) * log((N_i-N_ij)/N);
 			score += - ((N-N_j-N_i)/N) * log((N-N_j-N_i)/N);
@@ -112,7 +112,7 @@ float H2(int i, int j,
 	}
 	else
 	{
-		float score = 0.0;
+		double score = 0.0;
 		if (N_ij != 0)
 			score += -(N_ij/N) * log(N_ij/N);
 		if ((N_j-N_ij) != 0)
@@ -126,28 +126,40 @@ float H2(int i, int j,
 	}
 }
 
-float U(int i, int j, 
+double U(int i, int j, 
 		std::unordered_map<std::tuple<int,int>,int> *cooccurrences,
 		std::unordered_map<int,int> *occurrences,
 		int sentenceCount)
 {
-	float H_i = H1(i,occurrences,sentenceCount);
-	float H_j = H1(j,occurrences,sentenceCount);
+	double H_i = H1(i,occurrences,sentenceCount);
+	double H_j = H1(j,occurrences,sentenceCount);
 
-	float H_i_j = H2(i,j,cooccurrences,occurrences,sentenceCount);
+	double H_i_j = H2(i,j,cooccurrences,occurrences,sentenceCount);
 
-	float numerator = H_i + H_j - H_i_j;
-	float denominator = 0.5 * (H_i + H_j);
+	double numerator = H_i + H_j - H_i_j;
+	double denominator = 0.5 * (H_i + H_j);
 
-	//if (i<10 and j<10)
-	//	printf("DEBUG\t%d\t%d\t%f\t%f\t%f\t%f\t%f\t%f\n",i,j,H_i,H_j,H_i_j,numerator,denominator,numerator/denominator);
+	double score = 0.0;
+	if (denominator != 0)
+		score = numerator/denominator;
+
+	if (i==1153)
+	{
+
+		auto key = std::make_tuple ( min(i,j), max(i,j) );
+
+		int cooccurrence_ij = 0.0f;
+		if (cooccurrences->count(key) > 0)
+			cooccurrence_ij = (*cooccurrences)[key];
+		int occurrence_i = (*occurrences)[i];
+		int occurrence_j = (*occurrences)[j];
+
+		//printf("DEBUG\t%d\t%d\t%d\t%d\t%d\t%f\n",i,j,occurrence_i,occurrence_j,cooccurrence_ij,score);
+	}
 	
 	//printf("H2score=%f numerator=%f denominator=%f\n", H2score,numerator,denominator);
-	
-	if (denominator == 0)
-		return 0.0;
-	else
-		return numerator/denominator;
+
+	return score;
 }
 
 int main(int argc, char** argv)
@@ -231,18 +243,18 @@ int main(int argc, char** argv)
 		printf("vectorsToCalculate_array_size=%d\n",vectorsToCalculate_array_size);
 		printf("seen_size=%d\n",seen_size);
 		
-		float *allTmpData = (float*)malloc(omp_get_max_threads()*seen_size*sizeof(float));
+		double *allTmpData = (double*)malloc(omp_get_max_threads()*seen_size*sizeof(double));
 		
 		int complete = 0;
 		#pragma omp parallel for
 		for ( int i=0; i<vectorsToCalculate_array_size; i++ )
 		{
-			float *tmpData = &allTmpData[seen_size*omp_get_thread_num()];
+			double *tmpData = &allTmpData[seen_size*omp_get_thread_num()];
 			//int v1 = *it1;
 			int v1 = vectorsToCalculate_array[i];
 			//for ( auto it2 = occurrences.begin(); it2 != occurrences.end(); ++it2 )
 		
-			float norm2 = 0;
+			double norm2 = 0;
 			//fprintf(outIndexFile,"%d\n", v1);
 			for ( int j=0; j<seen_size; j++ )
 			{
@@ -253,7 +265,7 @@ int main(int argc, char** argv)
 				
 				
 				
-				float Uvalue = U(v1,v2, &cooccurrences, &occurrences, sentenceCount);
+				double Uvalue = U(v1,v2, &cooccurrences, &occurrences, sentenceCount);
 				
 				//printf("%d\t%d\t%f\n", v1, v2, Uvalue);
 				
@@ -270,7 +282,7 @@ int main(int argc, char** argv)
 
 			if (normalize == 1)
 			{
-				float norm = sqrt(norm2);
+				double norm = sqrt(norm2);
 				for ( int j=0; j<seen_size; j++ )
 					tmpData[j] /= norm;
 			}
@@ -291,7 +303,7 @@ int main(int argc, char** argv)
 				}
 				
 				fprintf(outIndexFile,"%d\n", v1);
-				fwrite(tmpData, sizeof(float), seen_size, outVectorFile);
+				fwrite(tmpData, sizeof(double), seen_size, outVectorFile);
 				complete++;
 			}
 			
